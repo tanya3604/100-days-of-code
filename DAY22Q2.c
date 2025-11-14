a6c1b106d9e9c79da7f2e449531212ceff35fce0
tanya3604 <tanyakeshwani1@gmail.com>
2025-11-15 04:03:57 +0530

    Add files via upload

---
+++ DAY22Q2.c
@@ -0,0 +1,15 @@
+// Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+
+*/
